692396d6fb158df0c9e38fe810265d12e9a3eb22
Bitfenix <Bitfenix@DESKTOP-H0MVHHV>
2023-01-31 10:32:56 -0600

    AndresAlvaFer - 1312023 - POO Practica

---
+++ POOEjemplo1/POOEjemplo1/Persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOEjemplo1
+{
+    internal class Persona
+    {
+        #region Atributos
+        
+        private String nombre, apellido;
+
+        #endregion
+
+        #region Sobrecarga y sobreescritura
+
+        public Persona()
+        {
+            this.nombre = "";
+            this.apellido = "";
+        }
+
+        public Persona(String pNom)
+        {
+            this.nombre = pNom;
+        }
+
+        public Persona(String pNom, String pApe)
+        {
+            this.nombre = pNom;
+            this.apellido = pApe;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public String Nombre
+        {
+
+            set { nombre = value.ToUpper(); }
+            get { return nombre; }
+        }
+
+        public String Apellido
+        {
+
+            set { apellido = value.ToUpper(); }
+            get { return apellido; }
+        }
+
+        #endregion
+    }
+}
--- POOEjemplo1/POOEjemplo1/Program.cs
+++ POOEjemplo1/POOEjemplo1/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Persona persona = new Persona();
+
+            for(int contador = 0; contador< 5; contador++)
+            {
+                Console.WriteLine("Ingrese el nombre de la persona:");
+                persona.Nombre = Console.ReadLine();
+                Console.WriteLine("el nombre ingresado es: " + persona.Nombre.ToString());
+            }
+            
+
+
+
         }
     }
 }
